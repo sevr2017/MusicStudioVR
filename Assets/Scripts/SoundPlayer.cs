dc74eb4d0de072ebe969b11772b304e700674bc2
LAPTOP-GGBJ45QO\alw <liujunyi>
2017-12-13 16:44:19 +0800

    recorder implemented; update the structure of the instrument by divide the play function into SoundPlayer.cs

---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour {
+    public AudioClip hitClip;
+    AudioSource drumAudio;
+
+    private void Awake()
+    {
+        drumAudio = GetComponent<AudioSource>();
+        drumAudio.clip = hitClip;
+
+    }
+   
+    public void play(float volume)
+    {
+       
+            drumAudio.volume = volume;
+            drumAudio.Play();
+        
+    }
+}
